acfd86158f3641cf357e0e8d4b9ef79dff6d2a0e
Erin <efeichtinger@mail.usf.edu>
2016-06-24 15:55:27 -0400

    Updated code written last July for construction of jay pedigree

--- JayPed.R
+++ JayPed.R
@@ -1,48 +1,59 @@
+### Picking up June 2016 - although ideally I want this all on the same script file
+
 #pedigree and kinship for Jays - for building kinship matrix for real (I hope)
 #July 2015 
-
-setwd("C:/Users/Erin/Dropbox/Jay survival analyses")
 
 library(kinship2)
 
 #upload file
-demo.ped <- read.csv(file="Ped_withlastobs.csv")
+demo.ped <- read.csv(file="jay_ped.csv", na.strings = c("","NA"))
+demo.ped <- na.omit(demo.ped)
 
 #change column names
-names(demo.ped)[names(demo.ped)=="Demo_Pedigree.Jays.USFWBand"] <- "ID"
-names(demo.ped)[names(demo.ped)=="Jays_1.USFWBand"] <- "MomID"
-names(demo.ped)[names(demo.ped)=="Jays_2.USFWBand"] <- "DadID"
+colnames(demo.ped)[1] <- "ID"
+colnames(demo.ped)[2] <- "IDband"
+colnames(demo.ped)[6] <- "MomID"
+colnames(demo.ped)[7] <- "Momband"
+colnames(demo.ped)[8] <- "DadID"
+colnames(demo.ped)[9] <- "Dadband"
+
 
 #convert dates to date format
 demo.ped$LastObsDate<- as.Date(demo.ped$LastObsDate, format = "%m/%d/%Y")
 
 #add censorship status column 
-demo.ped["censorship"] <- NA
-demo.ped$censorship <- 1
+demo.ped["censorship"] <- 1
 
-#add 0's to those still alive 2015-03-11
-demo.ped$censorship[which(demo.ped$LastObsDate=="2015-03-11")]<-0
-demo.ped$censorship[which(demo.ped$LastObsDate=="2015-04-07")]<-0
+#add 0's to those still alive 2016-4-12
+demo.ped$censorship[which(demo.ped$LastObsDate=="2016-4-12")]<-0
+
+#Subset the pedigree to get rid of birds with lastobsdate before 1981
+#Not sure if this is actually what I want to do, but I did it and it 
+#removed the NA's
+#demo.ped <- subset(demo.ped, demo.ped$LastObsDate >="1981-05-10")
 
 #add sex indicator column
-demo.ped["sexind"] <- 3
+demo.ped["sexind"] <- 0
 demo.ped$sexind[which(demo.ped$Sex=="M")] <- 1
 demo.ped$sexind[which(demo.ped$Sex=="F")] <- 2
 
 #add affected column (no meaning here, just needed for function)
 demo.ped["aff"] <- 0
 
-#have to do something with birds that have no parents listed (not in ID column
-# but in dad or mom id column, aka founders)
 demo.ped["missid"] <- "FALSE"
-demo.ped$missid[which(demo.ped$JayID=="LL-S")] <- "TRUE"
-demo.ped$missid[which(demo.ped$JayID=="SGY-")] <- "FALSE"
-demo.ped$missid[which(demo.ped$JayID=="-ORS")] <- "TRUE"
-demo.ped$missid[which(demo.ped$JayID=="SY-P")] <- "TRUE"
-demo.ped$missid[which(demo.ped$JayID=="-SYW")] <- "TRUE"
+
+### Need to specify the founders 
+
+jay.ped <- pedigree(id=demo.ped$IDband, dadid=demo.ped$Dadband, 
+                    momid=demo.ped$Momband, sex=demo.ped$sexind, 
+                    affected=demo.ped$aff, status=demo.ped$censorship, 
+                    missid = demo.ped$missid)
 
 
-jay.ped <- pedigree(id=demo.ped$ID, dadid=demo.ped$MBreeder, momid=demo.ped$FBreeder, sex=demo.ped$sexind, affected=demo.ped$aff, status=demo.ped$censorship, missid = demo.ped$missid)
+
+
+
+
 
 
 
