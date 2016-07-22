4fb20a766af4a9d4ab19befb759d96fad4404d14
Erin <efeichtinger@mail.usf.edu>
2016-07-22 13:37:32 -0400

    New script for breeders

---
+++ BreedersJuly.R
@@ -0,0 +1,36 @@
+#### Breeders
+#### July 21 2016
+
+## Read in file of breeders - known and unknown age
+
+brd <- read.csv("Breeders.csv")
+
+#remove duplicate rows 
+brd <- brd[!duplicated(brd),]
+
+colnames(brd)[1] <- "ID"
+colnames(brd)[2] <- "Band"
+colnames(brd)[3] <- "FY"
+colnames(brd)[4] <- "Fbreed"
+
+
+brd <- brd[,c(1,2,8,7,6,3,4,5)]
+
+#Set all blanks to NA
+brd[brd==""] <- NA
+#remove na
+brd <- na.omit(brd)
+
+brd$Fbreed <- as.Date(brd$Fbreed, format ="%m/%d/%Y")
+brd$LastObsDate <- as.Date(brd$LastObsDate, format = "%m/%d/%Y")
+
+#Years spent as a breeder, 365.25 to account for leap years 
+brd["Yrs"] <- (brd$LastObsDate - brd$Fbreed)/365.25
+brd$Yrs <- round(brd$Yrs, digits = 1)
+
+#Add censorship column
+brd["Censor"] <- 1
+brd$Censor[which(brd$LastObsDate =="2016-4-12")] <- 0
+
+brd <- subset(brd, brd$Yrs > 0)
+
